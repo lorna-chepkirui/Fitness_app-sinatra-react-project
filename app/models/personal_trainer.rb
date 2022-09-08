4e48472c4c7e2dc36a75343e1c44dee7ddc3b740
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-09-08 23:34:20 +0300

    created routines and personal trainer migrations

--- app/models/personal_trainer.rb
+++
@@ -1,8 +0,0 @@
-class PersonalTrainer < ActiveRecord::Base
-    has_many :routines
-    has_many :exercises, through: :routines
-
-
-
-    
-end
---
+++ db/migrate/20220908203240_create_personal_trainers.rb
@@ -0,0 +1,4 @@
+class CreatePersonalTrainers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
