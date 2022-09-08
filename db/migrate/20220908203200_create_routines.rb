3adf2b4bf08d0310fc6322fef893235ca4eba03d
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-09-08 23:45:38 +0300

    Created tables in the db

--- db/migrate/20220908203200_create_routines.rb
+++ db/migrate/20220908203200_create_routines.rb
@@ -1,4 +1,10 @@
 class CreateRoutines < ActiveRecord::Migration[6.1]
   def change
+    create_table :routines do |t|
+      t.string :routine_name
+      t.string :muscle_group
+      t.integer :personal_trainer_id
+      t.string :routine_image
+    end
   end
 end
--- db/migrate/20220908203609_create_exercises.rb
+++ db/migrate/20220908203609_create_exercises.rb
@@ -1,4 +1,10 @@
 class CreateExercises < ActiveRecord::Migration[6.1]
   def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :instructions
+      t.integer :personal_trainer_id
+      t.integer :routine_id
+    end
   end
 end
