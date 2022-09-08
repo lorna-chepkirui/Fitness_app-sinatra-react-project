10417572425d561bd70b467f47bb8993eebdaca2
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-09-08 23:48:02 +0300

    Modified the trainer's table

--- db/migrate/20220908203240_create_personal_trainers.rb
+++ db/migrate/20220908203240_create_personal_trainers.rb
@@ -1,4 +1,8 @@
 class CreatePersonalTrainers < ActiveRecord::Migration[6.1]
   def change
+    create_table :personal_trainers do |t|
+      t.string :name
+      t.string :gender
+    end
   end
 end
